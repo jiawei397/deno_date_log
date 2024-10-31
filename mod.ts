8451b8ed1f39ec74699c97d06253ec3e2be82310
jw397 <jw397@126.com>
2024-10-31 16:16:41 +0800

    style: lint

--- mod.ts
+++ mod.ts
@@ -1,7 +1,8 @@
 export * from "./src/date_file.ts";
-export { MyConsoleHandler } from "./src/console.ts";
+export * from "./src/console.ts";
 export * from "./src/main.ts";
+export * from "./src/date_format.ts";
 
 export type { DateFileLogConfig, Formatter, LogAppender } from "./src/types.ts";
 
-export { type LevelName, type LogRecord, getLogger } from "@std/log";
+export { getLogger, type LevelName, type LogRecord } from "@std/log";
